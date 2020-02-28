914eaa97ee57bcce65d47d01813790f431d7e29d
Thomas Bouffard <thomas.bouffard@bonitasoft.com>
2020-02-28 09:35:59 +0100

    tmp: start dedicated shape dev

---
+++ src/libs/bpmn-js/mxGraph/MxGraphBpmnShapes.ts
@@ -0,0 +1,17 @@
+import { mxgraph } from 'mxgraph'; // Typings only - no code!
+
+import { mxgraphFactory } from '../../../components/mxgraph-factory';
+
+// const { mxSh, mxPoint } = mxgraphFactory({
+//     mxLoadResources: false,
+//     mxLoadStylesheets: false,
+// });
+
+
+export default class MxGraphBpmnGatewayShape extends mxgraph.mxShape {
+
+
+    // constructor() {
+    //     super();
+    // }
+}
